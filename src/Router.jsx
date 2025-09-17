c66f7c934aa94c76ce22129c7e787cebfef79a11
Mary-G10 <merongeremew@yahoo.com>
2025-09-16 19:59:58 -0500

    added routing

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Auth/Landing/Landing/Landing";
@@ -8,11 +7,10 @@ import Orders from "./pages/Auth/orders/Orders";
 import Cart from "./pages/Auth/Cart/Cart";
 import Results from "./pages/Auth/Results/Results";
 import ProductDetail from "./pages/Auth/productDetail/ProductDetail";
-ProductDetail
 
 function Routing() {
   return (
-    <Router>
+    <Router basename="/Amazon-Clone-frontend-/">
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/auth" element={<Signup />} />
@@ -27,3 +25,31 @@ function Routing() {
 }
 
 export default Routing;
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Landing from "./pages/Auth/Landing/Landing/Landing";
+// import Signup from "./pages/Auth/Signup";
+// import Payment from "./pages/Auth/payment/Payment";
+// import Orders from "./pages/Auth/orders/Orders";
+// import Cart from "./pages/Auth/Cart/Cart";
+// import Results from "./pages/Auth/Results/Results";
+// import ProductDetail from "./pages/Auth/productDetail/ProductDetail";
+// ProductDetail
+
+// function Routing() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Landing />} />
+//         <Route path="/auth" element={<Signup />} />
+//         <Route path="/payment" element={<Payment />} />
+//         <Route path="/orders" element={<Orders />} />
+//         <Route path="/category/:categoryName" element={<Results />} />
+//         <Route path="/products/:productId" element={<ProductDetail />} />
+//         <Route path="/cart" element={<Cart />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default Routing;
