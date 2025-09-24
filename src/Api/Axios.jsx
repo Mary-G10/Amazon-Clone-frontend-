3e1c88296979c03dda46ee45e210a3d41265e217
Mary-G10 <merongeremew@yahoo.com>
2025-09-23 21:55:40 -0500

    deploying on netlify

--- src/Api/Axios.jsx
+++ src/Api/Axios.jsx
@@ -1,7 +1,10 @@
 import axios from "axios";
 export const productUrl = "https://fakestoreapi.com";
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-66248/us-central1/api",
+  // local instance of firebase funtions
+  // baseURL: "http://127.0.0.1:5001/clone-66248/us-central1/api",
+  // deployed version of amazon server on render.com
+  baseURL:"https://amazon-api-deploy-rzmi.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
